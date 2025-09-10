1b42fac7ae38ccb40cc8e84eb12d5c0dbb0dcf2b
Sarawoot-K <jays.sara27@gmail.com>
2025-09-11 02:27:42 +0700

    add imgProfile & content

--- src/App.jsx
+++ src/App.jsx
@@ -16,51 +16,62 @@ const App = () => {
     contact: Contact,
   };
 
-  const ActiveView = views[view]
+  const ActiveView = views[view];
 
   return (
-  <div>
     <div>
-      <section>
-        <ul className="flex justify-center p-3 gap-3">
-          <li>
-            <button onClick={() => setView("user")}
-              className="btn cursor-pointer"
+      <div className="flex flex-col items-center ">
+        <section className="relative w-150 h-30 flex justify-center items-center">
+          <ul className="flex justify-center p-3 gap-3 w-200">
+            <li>
+              <button
+                onClick={() => setView("user")}
+                className="btn cursor-pointer"
               >
-              Profile
-            </button>
-          </li>
-          <li>
-            <button onClick={() => setView("aboutme")}
-              className="btn cursor-pointer">
-              About Me
-            </button>
-          </li>
-          <li>
-            <button onClick={() => setView("skill")}
-              className="btn cursor-pointer">
-              Skil & Expreian
-            </button>
-          </li>
-          <li>
-            <button onClick={() => setView("myproject")}
-              className="btn cursor-pointer">
-              My Project
-            </button>
-          </li>
-          <li>
-            <button onClick={() => setView("contact")}
-              className="btn cursor-pointer">
-              Contact
-            </button>
-          </li>
-        </ul>
-      </section>
-      <section className="flex justify-center">
-        <ActiveView/>
-      </section>
+                Profile
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("aboutme")}
+                className="btn cursor-pointer"
+              >
+                About Me
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("skill")}
+                className="btn cursor-pointer"
+              >
+                Skil & Expreian
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("myproject")}
+                className="btn cursor-pointer"
+              >
+                My Project
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("contact")}
+                className="btn cursor-pointer"
+              >
+                Contact
+              </button>
+            </li>
+          </ul>
+        </section>
+        <section className="flex justify-center">
+          <div className="flex justify-center p-6 text-center min-h-250 max-w-900">
+            <ActiveView />
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
   );
 };
 
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
-
 const UserProfile = () => {
   return (
-    <div>Jay Sarawoot</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <div className="text-7xl">Jay Sarawoot</div>
+        </div>
+        <div className="flex justify-center p-4">
+          <img
+            src="/src/img/IMG_0296.JPG"
+            className="w-60 h-80 rounded-3xl"
+            alt="aaa"
+          ></img>
+        </div>
+        <div className="m-6 text-xl">
+          <p>Full Stack Developer</p>
+          <p>Science Tutor, Recreationist and Master of Ceremonies</p>
+        </div>
+        <div className="flex justify-center gap-4 p-6">
+          <div>link-in</div>
+          <div>line</div>
+          <div>facebook</div>
+          <div>e-mail</div>
+        </div>
+        <div className="text-2xl p-4">
+          <p>สวัสดีครับ, ผมเจ</p>
+          <p className="text-5xl p-3">ศราวุธ คำขาด</p>
+          <p>(Sarawoot Khumkhat)</p>
+          <p className="text-2xl p-3">
+            เว็บไซต์นี้เป็นช่องการการแชร์เส้นทางการทำงาน <br />
+            และการแบ่งปันความรู้ต่างๆของผมครับ
+          </p>
+        </div>
+        <div className="text-2xl">
+          <p>
+            รักการเรียนรู้สิ่งใหม่ๆ จนกลายเป็นเป็ด เป็ดที่ดีคือเป็ด
+            Pro(festional)
+          </p>
+        </div>
+      </div>
+      <div className="p-6 text-2xl">
+        <div>
+          <p>ทักษะและความเชี่ยวชาญ</p>
+        </div>
+        <div>
+          <p>
+            ผมเป็นโปรแกรมเมอร์มือใหม่ไฟแรงที่พร้อมลุยงาน
+            และพร้อมเรียนรู้เพื่อพัฒนาตนเองไปพร้อมๆกับองค์กร
+          </p>
+        </div>
+        <div>
+          <p>ที่อยู่</p>
+          <p>แพร่,ประเทศไทย</p>
+        </div>
+        <div>
+          <p>เป้าหมายชีวิต</p>
+          <p></p>
+        </div>
+      </div>
+      <div>
+        <p>ประสบการณ์</p>
+        <p>ประวัติการณ์ทำงาน</p>
+      </div>
+      <div>
+        <p>โปรเจค</p>
+        <p>ผลงาน</p>
+      </div>
+      <div>
+        <p>เทคโนโลยี</p>
+        <p>สิ่งที่ใช้</p>
+        <div>
+          <div>
+            <div>Business & Design</div>
+            <div>figma</div>
+          </div>
+          <div>
+            <div>Front-end</div>
+          </div>
+          <div>
+            <div>Back-end</div>
+          </div>
+          <div>
+            <div>Tool</div>
+            <div>Github</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
